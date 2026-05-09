3fc58f93a6ea3159f470674bcb3d119d287fdb01
Sofia Dominguez <135928742+snezhinka7@users.noreply.github.com>
2026-05-09 15:39:00 -0300

    push inicial con estructura y gitignore

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"Version: {version}");
+
